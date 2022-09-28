1d249ab12368645f8e353cf1a9b28b4d90f0fe55
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-09-27 19:16:41 -0500

    added classes and querySelectors and eventListeners for all buttons except <.> also added functions for clear, appendNum, setOperator

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
+let currValue = 0;
+let prevValue;
+
+
+const numBtns = document.querySelectorAll(".num");
+const opBtns = document.querySelectorAll(".op")
+const clrBtn = document.querySelector(".clr");
+const dltBtn = document.querySelector(".dlt");
+const currDisplay = document.querySelector(".curr-calc");
+const prevDisplay = document.querySelector(".prev-calc");
+
+
+numBtns.forEach((button) => button.addEventListener('click', () => appendNum(button.textContent)));
+opBtns.forEach((button) => button.addEventListener('click', () => setOperator(button.textContent)));
+clrBtn.addEventListener('click', () => clear());
+dltBtn.addEventListener('click', () => deleteNum());
 
 
 
 
+function setOperator(operator) {
+    prevValue = currValue + operator;
+    prevDisplay.textContent = prevValue;
+    currDisplay.textContent = "";
+}
 
 
 
 
+function deleteNum() {
+    //delete last digit in currdisplay and currvalue
+}
+
+function clear() {
+    currDisplay.textContent = "";
+    prevDisplay.textContent = "";
+}
 
 
 
-
-
-
-
+function appendNum(num) {
+    if(currDisplay.textContent.toString().length < 12){
+        if(currDisplay.textContent === "Error"){
+            currDisplay.textContent = "";
+        }
+        currDisplay.textContent += num;
+        currValue = currDisplay.textContent;
+    }
+    else{
+        currDisplay.textContent = "Error";
+    }
+}
 
 
 
